e1405f8260d21719910e9c88355191ffd358db3c
Svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2020-12-29 19:53:04 +0100

    Fixed package

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Qmusic",
+    name="qmusic",
     version="0.0.1",
     author="Joppe Wolters (svenskithesource)",
-    author_email="joppe.wolters@icloud.com",
+    author_email="joppe.wolters@icloud.com (discord: svenskithesource#2815)",
     description="An unofficial Qmusic API Wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/svenskithesource/qmusic",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("./qmusic"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
