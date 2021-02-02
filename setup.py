30e0d95329e75c627c1334287883fdf11a2593f4
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2021-02-02 17:44:16 +0100

    Build 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="qmusic",
-    version="0.0.1",
+    version="0.0.2",
     author="Joppe Wolters (svenskithesource)",
     author_email="joppe.wolters@icloud.com (discord: svenskithesource#2815)",
     description="An unofficial Qmusic API Wrapper.",
